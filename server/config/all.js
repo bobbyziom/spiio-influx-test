56df80545d3c6d4c6082e5400f23bec49ca61473
bobbyziom <bobbyziom@gmail.com>
2016-10-15 20:34:23 +0200

    secure

--- server/config/all.js
+++ server/config/all.js
@@ -3,9 +3,9 @@
 module.exports = {
 
 	tsdb: {
-		url: process.env.INFLUXDB_IRL || 'earthangel-a341ed15.influxcloud.net',
-		user: process.env.INFLUXDB_USER || 'spiiotsdb',
-		pw: process.env.INFLUXDB_PW || 'tsdbSpiio'
+		url: process.env.INFLUXDB_IRL,
+		user: process.env.INFLUXDB_USER,
+		pw: process.env.INFLUXDB_PW
 	},
 
 	port: 3000
